5ece996b207069679bf73966320b65bb4327b600
abdelkader ghanami <kaderghanami@gmail.com>
2023-06-18 07:21:40 -0700

    6

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 0;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar ('0' + n);
+	}
+putchar ('\n');
+return (0);
+}
